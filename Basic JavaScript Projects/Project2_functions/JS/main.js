16fc8a23dcd098f7ab4b52df5c83d961b3b27ae8
JemGodspeed <cemkadir_92@hotmail.com>
2024-09-26 21:51:49 -0400

    added project2_functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function my_first_function() {  
+    var str="this is the button text!!"
+    document.getElementById("button_text") .innerHTML=str;
+}
+
+function myfunction() {
+    var sentence="am i learning?";
+    sentence += "or am i not learning?";
+    document.getElementById("concatenate") .innerHTML = sentence;
+}
+    
+   
