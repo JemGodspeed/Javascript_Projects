0ff0c572cf5d3c0c3f40c9a17194349826be59b2
JemGodspeed <cemkadir_92@hotmail.com>
2024-09-26 21:27:54 -0400

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,11 @@
-document.write("hello world!")
+
+document.write("it is what it is" + ",or is it?")
+
+document.write(3+6)  //this is a comment example
+
+var sent1= "this is a begging of a string"
+var sent2= "and this is the end of a string"
+
+document.write(sent1+sent2)
+
+window.alert("Watch out!")
